236eab3a2912d948c1889972a561b1f71f77630d
Gustavo Morillo <gustavomorillo@gmail.com>
2023-01-30 18:01:00 -0400

    feat: :tada: First commit
    First commit to develop maxpet website

---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Mulish:wght@400;500;600;700&family=Work+Sans:wght@700&display=swap"
+            rel="preload"
+            as="style"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Mulish:wght@400;500;600;700&family=Work+Sans:wght@700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Image from 'next/image';
+import logo from '../public/logo.png';
+
+function HomePage() {
+  return <div>Home</div>;
+}
+
+export default HomePage;
